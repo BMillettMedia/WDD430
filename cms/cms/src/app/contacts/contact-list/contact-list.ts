301ffcae085490aabb55432a44357d1fc15e7605
Bryan Millett <bryanmillett@cox.net>
2026-01-16 20:48:20 -0700

    images added

--- cms/cms/src/app/contacts/contact-list/contact-list.ts
+++ cms/cms/src/app/contacts/contact-list/contact-list.ts
@@ -13,7 +13,7 @@ export class ContactListComponent {
       "R. Kent Jackson",
       "jacksonk@byui.edu",
       "208-496-3771",
-      "../../assets/images/jacksonk.jpg",
+      "assets/images/jacksonk.jpg",
       null
     ),
     new Contact(
@@ -21,7 +21,7 @@ export class ContactListComponent {
       "Rex Barzee",
       "barzeer@byui.edu",
       "208-496-3768",
-      "../../assets/images/barzeer.jpg",
+      "assets/images/barzeer.jpg",
       null
     )
   ];
