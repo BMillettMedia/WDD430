b329a6c44d56746da5245c7a979ae1d7e4f46678
Bryan Millett <bryanmillett@cox.net>
2026-01-24 18:27:26 -0700

    help o-o

--- cms/cms/src/app/messages/message-list/message-list.ts
+++ cms/cms/src/app/messages/message-list/message-list.ts
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './message-list.css',
 })
 export class MessageList {
-
+//sample messages copied from assignment image example 
+  messages = [
+    { sender: 'Bro. Jackson', text: 'The grades for this assignment have been posted' },
+    { sender: 'Steve Johnson', text: 'When is assignment 3 due' },
+    { sender: 'Bro. Jackson', text: 'Assignment 3 is due on Saturday at 11:30 PM' },
+    { sender: 'Mark Smith', text: 'Can I meet with you sometime. I need help with assignment 3' },
+    { sender: 'Bro. Jackson', text: 'I can meet with you today at 4:00 PM in my office.' }
+  ];
 }
