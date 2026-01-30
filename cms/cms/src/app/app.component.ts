a318d8a470260096630c56212e27732989086d08
Bryan Millett <bryanmillett@cox.net>
2026-01-29 20:18:57 -0700

    attempted redo

--- cms/cms/src/app/app.component.ts
+++ cms/cms/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'cms-root',
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
--- cms/cms/src/app/contacts/contacts.component.ts
+++ cms/cms/src/app/contacts/contacts.component.ts
@@ -5,17 +5,8 @@ import { ContactDetailComponent } from './contact-detail/contact-detail';
 
 @Component({
   selector: 'cms-contacts',
-  //standalone: true,
-  imports: [ContactListComponent, ContactDetailComponent],
-  template: `
-    <div class="row">
-      <div class="col-md-6">
-        <cms-contact-list></cms-contact-list>
-      </div>
-      <div class="col-md-6">
-        <cms-contact-detail></cms-contact-detail>
-      </div>
-    </div>
-  `
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css']
 })
+
 export class ContactsComponent { }
