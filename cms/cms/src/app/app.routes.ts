1380cc467602674be22228d6a07b0703cb0dd0a2
Bryan Millett <bryanmillett@cox.net>
2026-02-21 01:41:15 -0700

    i thnk it works

--- cms/cms/src/app/app.routes.ts
+++ cms/cms/src/app/app.routes.ts
@@ -1,11 +1,49 @@
 import { Routes } from '@angular/router';
-import { ContactsComponent } from './contacts/contacts.component';
-import { MessagesComponent } from './messages/messages.component';
+
+// Documents
 import { DocumentsComponent } from './documents/documents.component';
+import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
+import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
+
+// Messages
+import { MessagesComponent } from './messages/messages.component';
+import { MessageDetailComponent } from './messages/message-detail/message-detail.component';
+
+// Contacts
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactEditComponent } from './contacts/contact-edit/contact-edit.component';
 
 export const routes: Routes = [
-  { path: 'contacts', component: ContactsComponent },
-  { path: 'messages', component: MessagesComponent },
-  { path: 'documents', component: DocumentsComponent },
-  { path: '', redirectTo: '/contacts', pathMatch: 'full' }
+
+  { path: '', redirectTo: 'documents', pathMatch: 'full' },
+
+  {
+    path: 'documents',
+    component: DocumentsComponent,
+    children: [
+      { path: 'new', component: DocumentEditComponent },
+      { path: ':id', component: DocumentDetailComponent },
+      { path: ':id/edit', component: DocumentEditComponent }
+    ]
+  },
+
+  {
+    path: 'contacts',
+    component: ContactsComponent,
+    children: [
+      { path: 'new', component: ContactEditComponent },
+      { path: ':id', component: ContactDetailComponent },
+      { path: ':id/edit', component: ContactEditComponent }
+    ]
+  },
+
+  {
+    path: 'messages',
+    component: MessagesComponent,
+    children: [
+      { path: ':id', component: MessageDetailComponent }
+    ]
+  }
+
 ];
